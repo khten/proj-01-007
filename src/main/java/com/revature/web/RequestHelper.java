14dd9c0b7ca01c5e9638f3296e7aba8f71ec98f3
JustDamion <shirkeydamion@gmail.com>
2022-06-30 22:27:05 -0500

    merge with main

--- src/main/java/com/revature/web/RequestHelper.java
+++ src/main/java/com/revature/web/RequestHelper.java
@@ -12,7 +12,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import com.google.gson.JsonElement;
