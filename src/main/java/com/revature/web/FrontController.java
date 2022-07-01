f66d72408e02a429ade6ff730577a59d155903b6
JustDamion <shirkeydamion@gmail.com>
2022-07-01 00:02:44 -0500

    Changed registration, employees, admin bootstrap CSS

--- src/main/java/com/revature/web/FrontController.java
+++ src/main/java/com/revature/web/FrontController.java
@@ -33,89 +33,80 @@ public class FrontController extends HttpServlet {
 		// based on the URI returned
 		switch (URI) {
 
-		case "login":
-			RequestHelper.processLogin(request, response);
-			break;
+			case "login":
+				RequestHelper.processLogin(request, response);
+				break;
 
-		case "register":
-			RequestHelper.processRegistration(request, response);
-			break;
+			case "register":
+				RequestHelper.processRegistration(request, response);
+				break;
 
-		case "employees":
-			RequestHelper.processEmployees(request, response);
-			break;
+			case "employees":
+				RequestHelper.processEmployees(request, response);
+				break;
 
-		case "admin":
-			RequestHelper.processAdmin(request, response);
-			break;
+			case "admin":
+				RequestHelper.processAdmin(request, response);
+				break;
 
+			case "viewemps":
+				RequestHelper.processViewAllEmployees(request, response);
 
+				break;
 
-		case "viewemps":
-			RequestHelper.processViewAllEmployees(request, response);
+			case "approve_ticket":
 
-			break;
-
-		
-
-		case "approve_ticket":
-
-			RequestHelper.processApproveTicket(request, response);
-			break;
+				RequestHelper.processApproveTicket(request, response);
+				break;
 
 			case "deny_ticket":
 				RequestHelper.processDenyTicket(request, response);
 				break;
-				
+
 			case "pending_tickets":
 				RequestHelper.processTicketsByStatus(request, response, Status.Pending);
 				break;
-
 
 			case "tickets_by_username":
 				RequestHelper.processTicketsByUsername(request, response);
 
 				break;
 
-			
 			case "view_pending":
 				RequestHelper.processTicketsByStatus(request, response, Status.Pending);
 				break;
-				
+
 			case "view_approved":
 				RequestHelper.processTicketsByStatus(request, response, Status.Approved);
 				break;
-				
+
 			case "view_denied_tickets":
 				RequestHelper.processTicketsByStatus(request, response, Status.Denied);
 				break;
-				
 
 			case "approved_tickets":
 				RequestHelper.processTicketsByStatus(request, response, Status.Approved);
 				break;
-				
+
 			case "denied_tickets":
 				RequestHelper.processTicketsByStatus(request, response, Status.Denied);
 				break;
 
-			
+			case "resolved_tickets":
+				RequestHelper.processStatusResolved(request, response);
+				break;
 
-		case "resolved_tickets":
-			RequestHelper.processStatusResolved(request, response);
-			break;
+			case "all_pending_tickets":
+				RequestHelper.processAdminPending(request, response);
+				break;
 
-		case "all_pending_tickets":
-			RequestHelper.processAdminPending(request, response);
-			break;
+			case "all_resolved_tickets":
+				RequestHelper.processAdminResolved(request, response);
+				break;
 
-		case "all_resolved_tickets":
-			RequestHelper.processAdminResolved(request, response);
-			break;
-
-		default:
-			// TODO: Add custom error page
-			break;
+			default:
+				// TODO: Add custom error page
+				break;
 
 		}
 	}
